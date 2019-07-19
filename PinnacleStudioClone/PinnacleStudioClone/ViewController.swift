37fd681032efafebff48cc4cbbd5a9229900871b
Eric Blancas <ericblancas23@gmail.com>
2019-07-19 12:37:46 -0700

    completed

---
+++ PinnacleStudioClone/PinnacleStudioClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PinnacleStudioClone
+//
+//  Created by Eric Blancas on 7/18/19.
+//  Copyright © 2019 Eric Blancas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
